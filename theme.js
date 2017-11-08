b6e3e4da6835ebf095d76c60c264265771d6f95e
Luke Brooker <mail@lukebrooker.com>
2017-11-08 14:04:23 +1000

    Update current config

---
+++ theme.js
@@ -0,0 +1,38 @@
+exports.colors = {
+  dark: {
+    accent0: '#FA6B6B',
+    accent1: '#F89463',
+    accent2: '#FACE5F',
+    accent3: '#56D882',
+    accent4: '#1BAB6B',
+    accent5: '#2491BB',
+    accent6: '#9C8AD4',
+    accent7: '#EE638F',
+    shade0: '#203A44',
+    shade1: '#3F555E',
+    shade2: '#5E7078',
+    shade3: '#808E94',
+    shade4: '#9EAAAE',
+    shade5: '#BEC5C8',
+    shade6: '#DDE1E3',
+    shade7: '#ffffff'
+  },
+  light: {
+    accent0: '#FA6B6B',
+    accent1: '#F89463',
+    accent2: '#FACE5F',
+    accent3: '#56D882',
+    accent4: '#1BAB6B',
+    accent5: '#2491BB',
+    accent6: '#9C8AD4',
+    accent7: '#EE638F',
+    shade0: '#FFFCFF',
+    shade1: '#DDE1E3',
+    shade2: '#BEC5C8',
+    shade3: '#9EAAAE',
+    shade4: '#808E94',
+    shade5: '#5E7078',
+    shade6: '#3F555E',
+    shade7: '#203A44'
+  }
+}
